25239cc7dad86e8491c2441f22ca4cccc17023bf
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-02-11 20:55:07 +0300

    Add files via upload

--- 1.Svn/Client/PythonNetworkStreamCommand.cpp
+++ 1.Svn/Client/PythonNetworkStreamCommand.cpp
@@ -1,21 +1,11 @@
-//Find in : else if (!strcmpi(szCmd, "cube"))
-			if (3 > TokenVector.size())
-
-///Change
-#if defined(CUBE_RELOAD_FIX)
-			if (3+1 > TokenVector.size())
-#else
-			if (3 > TokenVector.size())
-#endif
-
 //Find
-			PyCallClassMemberFunc(m_apoPhaseWnd[PHASE_WINDOW_GAME], "BINARY_Cube_Open", Py_BuildValue("(i)", npcVNUM));
-			
-///Change
-#if defined(CUBE_RELOAD_FIX)
-			PyCallClassMemberFunc(m_apoPhaseWnd[PHASE_WINDOW_GAME], "BINARY_Cube_Open", Py_BuildValue("(ii)", npcVNUM, std::stoi(TokenVector[3])));
-#else
-			PyCallClassMemberFunc(m_apoPhaseWnd[PHASE_WINDOW_GAME], "BINARY_Cube_Open", Py_BuildValue("(i)", npcVNUM));
-#endif
+		else if ("close" == TokenVector[1])
+		{
+			PyCallClassMemberFunc(m_apoPhaseWnd[PHASE_WINDOW_GAME], "BINARY_Cube_Close", Py_BuildValue("()"));
+		}
+///Add
 
-https://puu.sh/F4z7Z/f59b8bd040.png
+#if defined(CUBE_RELOAD_FIX)
+		else if (!TokenVector[1].compare("reload"))
+			PyCallClassMemberFunc(m_apoPhaseWnd[PHASE_WINDOW_GAME], "BINARY_Reload_Cube", Py_BuildValue("()"));
+#endif
