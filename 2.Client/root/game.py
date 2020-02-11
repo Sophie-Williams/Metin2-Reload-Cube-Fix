6697657d50c448e0599ab3435146f38ecb703e40
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-02-11 20:54:48 +0300

    Add files via upload

--- 2.Client/root/game.py
+++ 2.Client/root/game.py
@@ -3,8 +3,7 @@
 		self.currentCubeNPC = npcVNUM
 
 #Change
-	def BINARY_Cube_Open(self, npcVNUM, reload = 0):
-		self.currentCubeNPC = npcVNUM
-		if app.CUBE_RELOAD_FIX and reload:
+	if app.CUBE_RELOAD_FIX:
+		def BINARY_Reload_Cube(self):
 			self.cubeInformation.clear()
 			self.interface.wndCube.ClearOldSlots()
